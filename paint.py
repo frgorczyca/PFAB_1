3f1c04a4a34774b2a1e2f0f9abb8e8e4e3fd33fa
Franciszek Gorczyca <franciszekgorczyca@MacBook-Pro.local>
2020-09-17 14:44:44 +0200

    PFAB_1 add basic brightness valuation, print chars according to brightness level

--- paint.py
+++ paint.py
@@ -1,6 +1,29 @@
+import math
 from PIL import Image
 
-im = Image.open("ascii-pineapple.jpg")
+im = Image.open("kacper.jpg")
 
-arr = im.load()
-print(arr[44,44])
+pixels = im.load()
+pixels_modified = {}
+pixels_asci = {}
+
+width, height = im.size
+
+def define_brightness(pixel):
+    return (pixel[0] + pixel[1] + pixel[2])/3
+
+for column in range(width):
+    for row in range(height):
+        pixels_modified[column, row] = (define_brightness(pixels[column, row]))
+
+chars = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+
+for column in range(width):
+    for row in range(height):
+        pixels_asci[column, row] = 3*chars[math.floor(
+            (pixels_modified[column, row] / 255) * (len(chars)-1))]
+
+for row in range(height):
+    for column in range(width):
+        print(pixels_asci[column, row], end="")
+    print()
